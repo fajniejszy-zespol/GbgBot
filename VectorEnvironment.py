e07fb755426a9cbe47f561468108d095cc009d60
mrbermell <31434035+mrbermell@users.noreply.github.com>
2020-11-03 09:47:06 +0100

    intermediate commit

--- VectorEnvironment.py
+++ VectorEnvironment.py
@@ -187,73 +187,21 @@ def worker(remote, parent_remote, env, worker_id):
     trainee = None 
     lectures = [] 
     worker_running = True 
-    
-    memory = WorkerMemory( """ BLA BLA BLA """)
-    
-    while trainee is None or len(lectures)==0: 
-        command, data = remote.recv()
-        if command == 'swap trainee': 
-            trainee = data
-        elif command == 'queue lecture': 
-            lecture.append(data)
-        elif command == 'close':
-            worker_running = False 
-            break 
-        else: 
-            raise Exception("Unknown command to worker")
-            exit()  
-    
-    lect = lectures.pop() 
-    obs = env.reset(lect)
-    
-    spatial_obs, non_spatial_obs = trainee._update_obs(obs)
-    memory.insert_first_obs(spatial_obs, non_spatial_obs)
-    
+    initialized = False 
     
     with torch.no_grad(): 
+        memory = WorkerMemory( """ TODO BLA BLA BLA """)
+        
+        lect = lectures.pop() 
+        obs = env.reset(lect)
+        
+        spatial_obs, non_spatial_obs = trainee._update_obs(obs)
+        memory.insert_first_obs(spatial_obs, non_spatial_obs)
         
         while worker_running:
             
-            # Agent takes step 
-            steps += 1
-            data_from_agent =  trainee.act(game=None, env=env, obs=obs)  
-            cnn_used_for_action = trainee.cnn_used_for_latest_action() 
-            
-            if cnn_used_for_action: 
-                (action, actions, action_masks, value) = data_from_agent
-                obs, reward, done, info = env.step(action)
-                # TODO, call for update_obs here  
-                reward_shaped = reward_function(env, info, shaped=True)
-                memory.insert_network_step(""" BLA BLA """)
-            else: 
-                pos = data_from_agent.position 
-                action  = {
-                    'action-type': data_from_agent.action_type,
-                    'x': None if pos is None else pos.x,
-                    'y': None if pos is None else pos.y } 
-                obs, reward, done, info = env.step(action)
-                # TODO, call for update_obs here  
-                reward_shaped = reward_function(env, info, shaped=True)
-                memory.insert_scripted_step(""" BLA BLA """)
-            
-            #Check progress and report back 
-            if done: 
-                
-                td_outcome = 0.5*(1 + info['touchdowns'] - info['opp_touchdowns'])
-                assert td_outcome in [0,0.5,1]
-                
-                memory.insert_epside_end( td_outcome=td_outcome ) 
-                
-                #handle lecture outcome  
-                obs = env.reset(lecture=lect)
-                spatial_obs, non_spatial_obs = trainee._update_obs(obs)
-                memory.insert_first_obs(spatial_obs, non_spatial_obs)
-    
-                steps = 0
-                remote.send((memory, lect))
-            
             # Updates from master process? 
-            while remote.poll():  
+            while remote.poll() or not initialized:  
                 command, data = remote.recv()
                 if command == 'swap trainee': 
                     trainee = data
@@ -265,6 +213,52 @@ def worker(remote, parent_remote, env, worker_id):
                 else: 
                     raise Exception("Unknown command to worker")
                     exit() 
+                
+                if not initialized: 
+                    initialized = not (trainee is None or len(lectures)==0) 
+            
+            
+            # Agent takes step 
+            steps += 1
+            data_from_agent =  trainee.act(game=None, env=env, obs=obs)  
+            cnn_used_for_action = trainee.cnn_used_for_latest_action() 
+            
+            if cnn_used_for_action: 
+                (action, actions, action_masks, value) = data_from_agent
+                obs, reward, done, info = env.step(action)
+                spatial_obs, non_spatial_obs = trainee._update_obs(obs)
+                
+                reward_shaped = reward_function(env, info, shaped=True)
+                memory.insert_network_step(""" TODO BLA BLA """)
+            else: 
+                pos = data_from_agent.position 
+                action  = {
+                    'action-type': data_from_agent.action_type,
+                    'x': None if pos is None else pos.x,
+                    'y': None if pos is None else pos.y } 
+                obs, reward, done, info, lect_outcome = env.step(action)
+                spatial_obs, non_spatial_obs = trainee._update_obs(obs)
+                
+                reward_shaped = reward_function(env, info, shaped=True)
+                memory.insert_scripted_step(""" TODO BLA BLA """)
+            
+            #Check progress and report back 
+            if done: 
+                
+                td_outcome = 0.5*(1 + info['touchdowns'] - info['opp_touchdowns'])
+                assert td_outcome in [0,0.5,1]
+                
+                memory.insert_epside_end( td_outcome=td_outcome ) 
+                
+                if len(lectures)>0: 
+                    lect = lectures.pop() 
+                
+                obs = env.reset(lecture=lect)
+                spatial_obs, non_spatial_obs = trainee._update_obs(obs)
+                memory.insert_first_obs(spatial_obs, non_spatial_obs)
+    
+                steps = 0
+                remote.send((memory, lect_outcome))
             
             if steps >= reset_steps:
                 # If we  get stuck or something - reset the environment
